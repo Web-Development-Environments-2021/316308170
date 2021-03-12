d64def5dc547fbcaed3c4ba7841a4436c44d15da
shiloari <shiloari@post.bgu.ac.il>
2021-03-12 18:26:08 +0200

    Changes: Photos, Contact
    Photos: changed gallery, fixed images size.
    Contact Me: fixed emojis select.

--- script.js
+++ script.js
@@ -23,9 +23,43 @@ function clickedImg(){
 
 function addEmoji(){
     document.getElementById("textArea").value += document.getElementById("emojis").value
+    document.getElementById("emojis").value = ''
 }
 
 function submit(){
     document.getElementById("textArea").value = ''
     document.getElementById("email").value = ''
+}
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
